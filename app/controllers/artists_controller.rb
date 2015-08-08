00a8c42fd2ff7f48df51b8e3c65880222a7c3646
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-08 15:50:04 -0600

    Able to parse artist pic, bio, related artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,14 +5,15 @@ class ArtistsController < ApplicationController
   # end
 
   def show
-    @artist = Echowrap.artist_search(name: params[:id])
-    # @artist = spotify_artists.find(params[:id])
-  end
+    @artist = Echowrap.artist_search(name: params[:id]).first
+    @artist_images = Echowrap.artist_images(name: @artist.name)  #use Spotify images... has 4 set images sizes...
+    @artist_bio = Echowrap.artist_biographies(name: @artist.name).first
+    # @artist_albums = Echowrap.artist_albums(name: @artist.name)     #user Spotify??
 
-  def related_artists
-    @artist.related_artists
-  end
+    @artist_news = Echowrap.artist_news(name: @artist.name).first
 
+    @related_artists = Echowrap.artist_similar(name: @artist.name, results: 20)
+  end
 
   private
 
