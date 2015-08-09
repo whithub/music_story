3308f331673a7d94b77c93fc7904ec794a9cd491
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-08 23:12:38 -0600

    Related artists section styled.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,18 +12,18 @@ class ArtistsController < ApplicationController
   def show
     @artist = Echowrap.artist_search(name: params[:id]).first
     @spotify_artist = RSpotify::Artist.search(params[:id]).first
-    # @profile_pic = @spotify_artist.images.third
+    @profile_pic = @spotify_artist.images.third
     @artist_images = Echowrap.artist_images(name: @artist.name)  #use Spotify images... has 4 set images sizes...
-    # @artist_bio = Echowrap.artist_biographies(name: @artist.name).first
+    @artist_bio = Echowrap.artist_biographies(name: @artist.name).first
     # @artist_albums = Echowrap.artist_albums(name: @artist.name)     #user Spotify??
 
-    # @artist_news = Echowrap.artist_news(name: @artist.name).first
-    # @videos = Echowrap.artist_video(name: @artist.name)
+    @artist_news = Echowrap.artist_news(name: @artist.name).first
+    @videos = Echowrap.artist_video(name: @artist.name)
 
     #@related_artists = Echowrap.artist_similar(name: @artist.name, results: 20)
-    @related_artists = @spotify_artist.related_artists.take(10)
+    @related_artists = @spotify_artist.related_artists.take(12)
 
-    # @websites = Echowrap.artist_urls(name: @artist.name)
+    @websites = Echowrap.artist_urls(name: @artist.name)
     @spotify_website = @spotify_artist.external_urls['spotify']
     @artist_thumbnail = @spotify_artist.images.last['url']
   end
