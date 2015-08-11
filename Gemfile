eeb484c1d6f95f0b73f1b71ac88b690c2c407500
Whitney Hiemstra <whitcolorado@gmail.com>
2015-08-10 22:55:00 -0600

    Setup caching

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'paperclip'
+gem 'dalli'
 
 group :development, :test do
   gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,4 +24,13 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.active_record.dump_schema_after_migration = false
+
+  config.cache_store = :dalli_store,
+    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+    {:username => ENV["MEMCACHIER_USERNAME"],
+      :password => ENV["MEMCACHIER_PASSWORD"],
+      :failover => true,
+      :socket_timeout => 1.5,
+      :socket_failure_delay => 0.2
+    }
 end
